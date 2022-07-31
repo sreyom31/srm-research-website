128b799830233a19c8f21be7aa26183940edd0f7
Vanshika Singh Gour <svanshika549@gmail.com>
2022-08-01 03:16:44 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 /* eslint-disable prettier/prettier */
 import './App.scss';
 import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
   //  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
   return (
-    <div className=''>
+    <div className="App">
       <Header/>
+      <div></div>
+      <div></div>
+      <Footer/> 
+      
     </div>
   );
 }
