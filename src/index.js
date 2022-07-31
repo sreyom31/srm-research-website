fce7cce13d8c942a73ea7bd7594bdd7a936ed3b3
Vanshika Singh Gour <svanshika549@gmail.com>
2022-07-31 23:15:35 +0530

    navbar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.scss';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <Router>
+    <Routes>
+      <Route path='/' element={<App/>}/>
+    </Routes>
+  </Router>,
+    document.getElementById('root')
 );
