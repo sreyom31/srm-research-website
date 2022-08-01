1f9a3e36593c7d4bc44576ce74a576856be207b3
ProMinato500 <nandip500@gmail.com>
2022-08-01 22:01:37 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable prettier/prettier */
 import './App.scss';
-// import Header from './components/Header.jsx'
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
 import Homepage from './pages/Homepage.js'
 
 function App() {
   //  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
   return (
-    <div className=''>
-      {/* <Header/> */}
+    <div className='App'>
       <Homepage />
     </div>
   );
