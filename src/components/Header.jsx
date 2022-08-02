1552abcd08ad1d5d56059b4f0fb850a779346b64
Vanshika Singh Gour <svanshika549@gmail.com>
2022-08-03 02:16:21 +0530

    changed navbar colour

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,35 +13,30 @@ const Header = () => {
     }
 
   return (
-    <div className='fixed w-full absolute top-0 text-white flex justify-between p-4 items-center bg-black '>
-
-        <div className='text-2xl font-bold text-center uppercase'>
-            <h1> SRM <span className='block text-4xl'>ICIoT - 2023</span></h1>
+    <div className='fixed w-full absolute top-0 text-white flex justify-between p-4 items-center bg-sky-600'>
+        <div className='text-xl font-bold text-center'>
+            <h1> SRM <span className='block text-2xl'>ICIoT - 2023</span></h1>
         </div> 
 
         <nav>
-
             <div className='absolute right-6 md:hidden top-6 scale-150'>
-                <MenuOutlined onClick={showMenu} className='scale-150 cursor-pointer'/>
+                <MenuOutlined onClick={showMenu} className='scale-100 cursor-pointer'/>
             </div>
                 <ul className='hidden md:flex gap-8 p-6 uppercase'>
-                    <li className="text-gray-300 hover:bg-gray-700 hover:text-white active:text-white px-4 py-2 rounded-md font-medium">
+                    <li className="text-gray-300 hover:bg-blue-400 hover:text-white px-4 py-2 rounded-md font-medium">
                         <Link to='/'>Home</Link>
                     </li>
-                    <li className="text-gray-300 hover:bg-gray-700 hover:text-white active:text-white px-4 py-2 rounded-md font-medium">
+                    <li className="text-gray-300 hover:bg-blue-400 hover:text-white px-4 py-2 rounded-md font-medium">
                         <Link to='/'>Conferences</Link>
                     </li>
-                    <li className="text-gray-300 hover:bg-gray-700 hover:text-white active:text-white px-4 py-2 rounded-md font-medium">
+                    <li className="text-gray-300 hover:bg-blue-400 hover:text-white px-4 py-2 rounded-md font-medium">
                         <Link to='/speakers'>Speakers</Link>
                     </li>
-                    <li className="text-gray-300 hover:bg-gray-700 hover:text-white active:text-white px-4 py-2 rounded-md font-medium">
+                    <li className="text-gray-300 hover:bg-blue-400 hover:text-white px-4 py-2 rounded-md font-medium">
                         <Link to='/'>Gallery</Link>
                     </li>
-                    <li className="text-gray-300 hover:bg-gray-700 hover:text-white active:text-white px-4 py-2 rounded-md font-medium">
+                    <li className="text-gray-300 hover:bg-blue-400 hover:text-white px-4 py-2 rounded-md font-medium">
                         <Link to='/'>Register</Link>
-                    </li>
-                    <li className="text-gray-300 hover:bg-gray-700 hover:text-white active:text-white px-4 py-2 rounded-md font-medium">
-                        <Link to='/'>Login</Link>
                     </li>
                 </ul>
 
--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -6,25 +6,22 @@ import { Link } from 'react-router-dom';
 
 const MenuItems = ({ showMenu, active }) => {
     return (
-        <ul className={active ? 'flex-col flex items-center fixed inset-0 left-1/4 uppercase bg-black backdrop-blur-lg gap-8 justify-center p-8 md:hidden' : 'hidden'}>
+        <ul className={active ? 'flex-col flex items-center fixed inset-0 left-1/4 uppercase bg-sky-600 backdrop-blur-lg gap-8 justify-center p-8 md:hidden' : 'hidden'}>
             <Close onClick={showMenu} className='cursor-pointer' />
-            <li className="text-gray-300 hover:bg-gray-700 hover:text-white hover:display-block px-3 py-2 rounded-md text-base font-medium">
+            <li className="text-gray-300 hover:bg-blue-400 hover:text-white px-4 py-2 rounded-md text-base font-medium">
                 <Link to='/'>Home</Link>
             </li>
-            <li className="text-gray-300 hover:bg-gray-700 hover:text-white active:text-white px-4 py-2 rounded-md font-medium">
+            <li className="text-gray-300 hover:bg-blue-400 hover:text-white px-4 py-2 rounded-md font-medium">
                 <Link to='/'>Conferences</Link>
             </li>
-            <li className="text-gray-300 hover:bg-gray-700 hover:text-white active:text-white px-4 py-2 rounded-md font-medium">
+            <li className="text-gray-300 hover:bg-blue-400 hover:text-white px-4 py-2 rounded-md font-medium">
                 <Link to='/'>Speakers</Link>
             </li>
-            <li className="text-gray-300 hover:bg-gray-700 hover:text-white active:text-white px-4 py-2 rounded-md font-medium">
+            <li className="text-gray-300 hover:bg-blue-400 hover:text-white px-4 py-2 rounded-md font-medium">
                 <Link to='/'>Gallery</Link>
             </li>
-            <li className="text-gray-300 hover:bg-gray-700 hover:text-white active:text-white px-4 py-2 rounded-md font-medium">
+            <li className="text-gray-300 hover:bg-blue-400 hover:text-white px-4 py-2 rounded-md font-medium">
                 <Link to='/'>Register</Link>
-            </li>
-            <li className="text-gray-300 hover:bg-gray-700 hover:text-white active:text-white px-4 py-2 rounded-md font-medium">
-                <Link to='/'>Login</Link>
             </li>
         </ul>
     );
