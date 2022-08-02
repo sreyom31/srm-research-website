a9918b056de45a0ecca9229967eb1dc493e53fb5
ProMinato500 <nandip500@gmail.com>
2022-08-02 11:34:40 +0530

    responsive and corrected files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 /* eslint-disable prettier/prettier */
 import './App.scss';
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
+// import Header from './components/Header.jsx'
+// import Footer from './components/Footer.jsx'
+import Homepage from './pages/Homepage.js'
 
 function App() {
   //  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
   return (
     <div className="App">
-      <Header/>
-      <Footer/> 
+      <Homepage />
     </div>
   );
 }
