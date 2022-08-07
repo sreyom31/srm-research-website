2f5c3816b234c757d50008cd13136c2672964960
vanshika-singh518 <svanshika549@gmail.com>
2022-08-07 13:05:34 +0530

    updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,51 +1,44 @@
 /* eslint-disable prettier/prettier */
-import { MenuOutlined } from '@material-ui/icons';
-import React,{useState} from 'react';
-import { Link } from 'react-router-dom';
-import MenuItems from './MenuItems';
+import React from 'react';;
+import Navbar from '../components/Nav';
+import srmlogo from '../../src/images/srmlogo.png'
+import elsev from '../../src/images/elsevier-logo.png'
+import daytonlogo from '../../src/images/DaytonLogo.png'
+
 
 const Header = () => {
 
-    const [active,setActive] = useState(false)
-
-    const showMenu = () => {
-        setActive(!active)
-    }
-
-  return (
-    <div className='fixed w-full absolute top-0 text-white flex justify-between p-4 items-center bg-sky-600'>
-        <div className='text-xl font-bold text-center'>
-            <h1> SRM <span className='block text-2xl'>ICIoT - 2023</span></h1>
-        </div> 
-
-        <nav>
-            <div className='absolute right-6 md:hidden top-6 scale-150'>
-                <MenuOutlined onClick={showMenu} className='scale-100 cursor-pointer'/>
+    return (
+        <div>
+            <div className='grid grid-cols-3 justify-items-center'>
+                <img src={srmlogo} className='w-[40%]' ></img>
+                <img src={elsev} className='w-[25%]'></img>
+                <img src={daytonlogo} className='w-[20%]'></img>
             </div>
-                <ul className='hidden md:flex gap-8 p-6 uppercase'>
-                    <li className="text-gray-300 hover:bg-blue-400 hover:text-white px-4 py-2 rounded-md font-medium">
-                        <Link to='/'>Home</Link>
-                    </li>
-                    <li className="text-gray-300 hover:bg-blue-400 hover:text-white px-4 py-2 rounded-md font-medium">
-                        <Link to='/'>Conferences</Link>
-                    </li>
-                    <li className="text-gray-300 hover:bg-blue-400 hover:text-white px-4 py-2 rounded-md font-medium">
-                        <Link to='/speakers'>Speakers</Link>
-                    </li>
-                    <li className="text-gray-300 hover:bg-blue-400 hover:text-white px-4 py-2 rounded-md font-medium">
-                        <Link to='/'>Gallery</Link>
-                    </li>
-                    <li className="text-gray-300 hover:bg-blue-400 hover:text-white px-4 py-2 rounded-md font-medium">
-                        <Link to='/'>Register</Link>
-                    </li>
-                </ul>
-
-                <MenuItems showMenu={showMenu} active={active}/>
-
-        </nav>
-
-    </div>
-  );
+            <br />
+            <div className='container text-center'>
+                <div className='w-17/20 mx-auto font-bold'>
+                    <p className='h-18 text-2xl text-red-900'>International Conference on Internet of Things</p>
+                    <h1 className='h-15 pt-5 pb-5 text-3xl text-red-900'>ICIot 2023</h1>
+                    <div className='font-serif text-2xl text-black'>07 - 08 September 2023</div>
+                    <h5 className='h-10 text-center text-red-900 text-xl font-medium'>SRMIST, KTR, Chennai, India</h5>
+                    <h4 className='text-center title text-blue-500 text-xl'>
+                        <p>
+                            <a href='/'>All accepted papers will be published in Procedia Computer Science Journal, Elsevier</a>
+                        </p>
+                    </h4>
+                </div>
+            </div>
+            <br />
+            <div className='bg-red-600'>
+                <marquee behavior='alternate' className='text-white bg-red-600'>
+                    <a href='/'>  All accepted papers will be published in Procedia Computer Science Journal, Elsevier.</a>
+                    <span className='ml-20'>Last Date for Paper Submission: 15th June 2023</span>
+                </marquee>
+            </div>
+            <Navbar />
+        </div>
+    );
 };
 
 export default Header;
