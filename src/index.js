a53d961a5c80a5ebaa380856b093a8d30f04cd95
vanshika-singh518 <svanshika549@gmail.com>
2022-08-09 16:21:09 +0530

    Added Gallery

--- src/index.js
+++ src/index.js
@@ -8,12 +8,15 @@ import Info from './components/Info';
 import Submission from './components/Submission';
 import Registration from './components/Registration/Registration';
 import Homepage from './pages/Homepage';
+import Gallery from './components/Gallery'
+
 ReactDOM.render(
   <Router>
     <Routes>
       <Route path='/' element={<App/>}/>
       <Route path='/speakers' element={<Info/>}/>
-      <Route path='/submission' element={<Submission/>}/>
+      <Route path='/submission' element={<Submission />} />
+      <Route path='/gallery' element={<Gallery/>} />
       <Route path="/registration" element={<Registration/>}/>
       <Route path="/home" element={<Homepage/>}/>
     </Routes>
